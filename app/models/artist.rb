ca78ed8b2487ea12a6d71f411d4a90b4f788e326
Emilio <emiliopalm@gmail.com>
2021-07-03 19:09:20 -0300

    Cree modelo Artist, funcionando.

---
+++ app/models/artist.rb
@@ -0,0 +1,5 @@
+class Artist < ApplicationRecord
+    validates :name, presence: {message: "Debe tener nombre."}, length: {in: 3..30}
+    validates :biography, presence: {message: "Debe tener una biografía copada."}, length: {in: 3..100}
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_214822) do
+ActiveRecord::Schema.define(version: 2021_07_03_213417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2021_07_01_214822) do
     t.string "album_art"
     t.integer "total_duration"
     t.jsonb "cached_songs"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.text "biography"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
