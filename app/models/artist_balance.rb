a11bd5c366445baf14dd81ecb4521e44ac80a18c
Emilio <emiliopalm@gmail.com>
2021-07-05 11:38:16 -0300

    Cree validaciones para ArtistBalance

--- app/models/artist_balance.rb
+++ app/models/artist_balance.rb
@@ -1,3 +1,5 @@
 class ArtistBalance < ApplicationRecord
   belongs_to :artist
+  validates :artist_id, numericality: {message: "%{value} Debe ser el ID de un Artista."}
+  validates :balance, numericality: {message: "%{value} Debe ser un decimal."}
 end
