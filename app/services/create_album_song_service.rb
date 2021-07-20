8ea00f82d3a2196221d002b0f11277056b3e21ad
Emilio <emiliopalm@gmail.com>
2021-07-19 21:48:37 -0300

    Cree la suma de total_duration y duration en CreateAlbumSongService

--- app/services/create_album_song_service.rb
+++ app/services/create_album_song_service.rb
@@ -1,5 +1,6 @@
 class CreateAlbumSongService
     attr_reader :album_id, :song_id
+    attr_accessor :total_duration
 
     def initialize(album_id, song_id)
         @album_id = album_id
@@ -12,8 +13,10 @@ class CreateAlbumSongService
                 album_find = Album.find_by id: @album_id
                 raise StandardError, 'El Album no existe' unless album_find.present?
 
-                AlbumSong.create(album_id: @album_id, song_id: @song_id)  
-
+                AlbumSong.create(album_id: @album_id, song_id: @song_id) 
+                
+                @suma = Album.find(@album_id).total_duration + Song.find(@song_id).duration
+                Album.find(@album_id).update(total_duration: @suma)
             end
         end
     end
